cfe9b6cfe1f420e0f8cf651634274f89e971d82c
Gangaraju99 <iamraju1411@gmail.com>
2023-06-20 17:24:59 +0530

    Validations

--- src/main/java/com/mse/Application.java
+++ src/main/java/com/mse/Application.java
@@ -1,8 +1,5 @@
 package com.mse;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
